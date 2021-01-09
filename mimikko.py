749c915da961be13a57e8bcce9573709b941198f
Adam·Eva <39186981+cyb233@users.noreply.github.com>
2021-01-09 20:30:16 +0700

    Update mimikko.py

--- mimikko.py
+++ mimikko.py
@@ -7,6 +7,7 @@ import sys
 import time
 import requests
 import logging
+import re
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
