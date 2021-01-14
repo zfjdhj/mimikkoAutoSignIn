003d25323c74960cc21d9e926d7814429be415a5
Administrator <320336328@qq.com>
2021-01-14 16:01:03 +0800

    test

--- README.md
+++ README.md
@@ -8,7 +8,32 @@
 
 2. 创建action secrets
 `secrets.APP_ID`,`secrets.AUTHORIZATION`;
+`可选功能`日志输出到文件，需要设定`secrets.SECRET_TOKEN`
+
+3. 修改config.json
+
+``` json
+
+
+```
 
 ## Hoshino插件版
 
 项目地址<https://github.com/zfjdhj/zfjbot-mimikko>
+
+## 说明
+
+这只是一个脚本，不要期望有太多功能。
+
+或者使用大佬的另一版本
+
+<https://github.com/cyb233/mimikkoAutoSignIn>
+
+## 更新
+
+1. 新增日志输出到文件，需要设定secrets.SECRET_TOKEN
+**注意：**脚本包含上传功能，已关闭action push触发，如有需要请自行开启（会套娃），测试完毕后尽快关闭
+
+2. 新增config.json用于设定助手
+
+3. 感谢cyb233大佬提供的建议
--- mimikko.py
+++ mimikko.py
@@ -6,30 +6,71 @@
 import sys
 import time
 import requests
+import json
+import os
+import logging
+from logging import handlers
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-try:
-    if len(sys.argv)==3:
-        app_id = sys.argv[1]
-        Authorization=sys.argv[2]
-    else:
-        print("缺少必要参数！！！(Bot插件版忽略此错误)")
-        # 也可以在这里设定默认值
-        app_id = ""
-        Authorization=""
-except Exception as e:
-    print(e)
+base_path=os.path.dirname(__file__)
+
+class Logger(object):
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+    }#日志级别关系映射
+
+    def __init__(self,filename,level='info',when='D',backCount=30,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')
+        #往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
+
+log = Logger(base_path+'/log/all.log',level='debug')
 
 
+# log.logger.debug('debug 信息')
+# log.logger.info('info 信息')
+# log.logger.warning('warning 信息')
+# log.logger.error('error 信息')
+# log.logger.critical('critial 信息')
+
+if len(sys.argv)==3:
+    app_id = sys.argv[1]
+    Authorization=sys.argv[2]
+else:
+    log.logger.warning("缺少必要参数！！！(Bot插件版忽略此错误)")
+    # 也可以在这里设定默认值
+    app_id = ""
+    Authorization=""
+with open(base_path+"/config.json","r") as f:
+    config=json.loads(f.read(),encoding='utf8')
+if config["code"] == "":
+    config["code"]="momona"
+    log.logger.warning("config-code为空，自动设置签到助手为梦梦奈")
 
 # id=sys.argv[3]
 # password=sys.argv[4]
 
 apiPath = 'http://api1.mimikko.cn/client/user/GetUserSignedInformation'
 apiPath2 = 'http://api1.mimikko.cn/client/dailysignin/log/30/0'
-# post_data = {"password": password, "id": id}
 sign_path = 'https://api1.mimikko.cn/client/RewardRuleInfo/SignAndSignInformationV3'
 energy_info_path = 'https://api1.mimikko.cn/client/love/GetUserServantInstance'
 energy_reward_path = 'https://api1.mimikko.cn/client/love/ExchangeReward'
@@ -65,7 +106,7 @@ def apiRequest(url,app_id,Authorization,params):
             return res
 
     except Exception as ex:
-        print(ex)
+        log.logger.error(ex)
 
 
 # code=momona,ServantName=梦梦奈
@@ -76,11 +117,11 @@ def apiRequest(url,app_id,Authorization,params):
 
 def mimikko(app_id,Authorization):
     sign_data = apiRequest(sign_path,app_id,Authorization,"")
-    energy_info_data = apiRequest(energy_info_path,app_id,Authorization,{"code": "ruri"})
+    energy_info_data = apiRequest(energy_info_path,app_id,Authorization,{"code":config["code"]})
     if energy_info_data:
         if energy_info_data.get('body'):
             if energy_info_data['body']['Energy'] > 0:
-                energy_reward_data = apiRequest(energy_reward_path, app_id,Authorization,{"code": "ruri"})
+                energy_reward_data = apiRequest(energy_reward_path, app_id,Authorization,{"code": config["code"]})
             else:
                 energy_reward_data = "您的能量值不足，无法兑换"
         else:
@@ -99,7 +140,7 @@ def timeStamp2time(timeStamp):
 if app_id and Authorization:
     sign_data, energy_info_data, energy_reward_data, sign_info, sign_history = mimikko(app_id,Authorization)
     # # sign_data
-    print('sign_data', sign_data)
+    log.logger.info('sign_data:\n'+str(sign_data))
     # print("code", sign_data["code"])
     # # print(sign_data["body"]["date"])
     # # print(sign_data["body"]["signTime"])
@@ -109,7 +150,7 @@ if app_id and Authorization:
     # print('成长值Reward', sign_data["body"]['Reward'])
     # print('硬币GetCoin', sign_data["body"]['GetCoin'])
     # # Energy info
-    print('energy_info_data', energy_info_data)
+    log.logger.info('energy_info_data:\n'+str(energy_info_data))
     # print('code', energy_info_data['code'])
     # print('msg', energy_info_data['msg'])
     # # print('Favorability',energy_data['body']['Favorability'])
@@ -118,14 +159,14 @@ if app_id and Authorization:
     #       str(energy_info_data['body']['Favorability']) + "/" + str(energy_info_data['body']['MaxFavorability']))
     # print('Energy', energy_info_data['body']['Energy'])
     # # Energy reward
-    print(energy_reward_data)
+    log.logger.info("energy_reward_data:\n"+str(energy_reward_data))
     # # sign_info
     # print(sign_info)
     # print(sign_info['code'])
     # print('IsSign', sign_info['body']['IsSign'])
     # print('连续登录天数ContinuousSignDays', sign_info['body']['ContinuousSignDays'])
     # # sign_history
-    print(sign_history)
+    log.logger.info("sign_history:\n"+str(sign_history))
     # print('code', sign_history['code'])
     # print('startTime', timeStamp2time(sign_history["body"]['startTime']))
     # print('endTime', timeStamp2time(sign_history["body"]['endTime']))
