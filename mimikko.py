c73539e0e60c64903dd5f74db533e224d4f40b02
Schwi <39186981+cyb233@users.noreply.github.com>
2021-03-19 14:45:18 +0800

    Update mimikko.py

--- mimikko.py
+++ mimikko.py
@@ -171,8 +171,8 @@ def timeStamp2time(timeStamp):
 
 def timeStamp2sign(DDSECRET):
     timestamp = str(round(time.time() * 1000))
-    secret_enc = DDKEY.encode('utf-8')
-    string_to_sign = '{}\n{}'.format(timestamp, DDKEY)
+    secret_enc = DDSECRET.('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, DDSECRET)
     string_to_sign_enc = string_to_sign.encode('utf-8')
     hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
     sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
